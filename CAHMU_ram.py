227805c608ff82c98e9e65fa970716d20fb44f71
ClownOG <clownpriest8@gmail.com>
2025-09-19 16:43:02 +0600

    CAHMU_ram.py

--- CAHMU_ram.py
+++ CAHMU_ram.py
@@ -33,8 +33,8 @@ active = True
 left_pressed = False
 right_pressed = False
 running = True
-tick_ms = 0  # 14 ms between moves
-vertical_strength = 6  # constant pull
+tick_ms = 14  # small delay between moves
+vertical_strength = 8  # constant pull (set to 8x)
 
 mouse_controller = MouseController()
 
@@ -97,5 +97,3 @@ threading.Thread(target=icon.run, daemon=True).start()
 # ------------------------
 while running:
     time.sleep(0.2)
-
-
